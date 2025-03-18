6ad837b97b203ee9d67b3aa0fe60e22f27934b2c
klerbraian <braiankler.30@gmail.com>
2025-03-18 12:44:15 -0300

    cv

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,14 +10,14 @@ const {textData} = useContext(LanguageContext)
         <div className="background-about">
             <div className='container-about'>
                 <div className='image-container'>
-                    <img className='image' src="/img/perfil.jpg" alt="" />
+                    <img className='image' src="\public\img\perfil4x.jpeg" alt="" />
                 </div>
                 <div className='about-text'>
                     <h3 className='about-h3'>{textData?.["about-me"]?.title || "About me"}</h3>
                     <p className='about-p'>{textData?.["about-me"]?.text || "Hi 👋, I'm Braian Kler, born in Argentina. I am a funny and determined developer to create efficient and functional solutions. I have trained in frontend and backend technologies. I am looking for opportunities to contribute to innovative projects, improve my skills and continue learning in the world of web development. Open to new challenges and experiences."}
                     </p>
                     <Contacto />
-                    <p className='curriculum'>Curriculum vitae: <a className='redes-link' href="assets/curriculum.pdf" download="Curriculum_Braiankler"><i class="fi fi-br-download"></i></a></p>
+                    <p className='curriculum'>Curriculum vitae: <a className='redes-link' href="\tools\Curriculum Braian.doc (1).docx" download="Curriculum_Braiankler"><i class="fi fi-br-download"></i></a></p>
                 </div>
             </div>
         </div>
